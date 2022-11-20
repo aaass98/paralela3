3ece352c137e9f132a1e0ce588a71fef36a562a6
aaass98 <leopoldosantos98@hotmail.com>
2022-11-20 10:47:10 -0400

    Update HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -36,63 +36,72 @@ int main(int argc, char** argv) {
 		MPI_Finalize();
 		exit(1);
 	}
-	printf(argv[1]);
-	printf(argv[2]);
 
+	if (pId == 0) {
+		// Abre arquivo de entrada
+		arqIn = fopen(argv[1], "rt");
 
-	// Abre arquivo de entrada
-	arqIn = fopen(argv[1], "rt");
-
-	if (arqIn == NULL)
-	{
-		if (pId == 0) {
-			printf("\nArquivo texto de entrada não encontrado\n");
+		if (arqIn == NULL)
+		{
+			if (pId == 0) {
+				printf("\nArquivo texto de entrada não encontrado\n");
+			}
+			MPI_Finalize();
+			exit(1);
 		}
-		MPI_Finalize();
-		exit(1);
-	}
 
-	// Lê tamanho do vetor de entrada
-	fscanf(arqIn, "%d", &n);
+		// Lê tamanho do vetor de entrada
+		fscanf(arqIn, "%d", &n);
 
-	// Aloca vetores de entrada e saída
-	vIn = (int*)malloc(n * sizeof(int));
-	if (vIn == NULL)
-	{
-		if (pId == 0) {
-			printf("\nErro na alocação de estruturas\n");
+		// Aloca vetor de entrada
+		vIn = (int*)malloc(n * sizeof(int));
+		if (vIn == NULL)
+		{
+			if (pId == 0) {
+				printf("\nErro na alocação de estruturas\n");
+			}
+			MPI_Finalize();
+			exit(1);
 		}
-		MPI_Finalize();
-		exit(1);
-	}
 
-	// Lê vetor do arquivo de entrada
-	for (i = 0; i < n; i++)
-	{
-		fscanf(arqIn, "%d", &(vIn[i]));
-	}
+		// Lê vetor do arquivo de entrada
+		for (i = 0; i < n; i++)
+		{
+			fscanf(arqIn, "%d", &(vIn[i]));
+		}
 
-	// Fecha arquivo de entrada
-	fclose(arqIn);
+		// Fecha arquivo de entrada
+		fclose(arqIn);
+	}
 
 	// -------------------------------------------------------------------------
 	// Corpo principal do programa
 
-	// TODO: Medir tempo inicial
+
 
 	// Remove 0s do vetor de entrada, produzindo vetor de saída
 	if (pId == 0) {
+		// TODO: Medir tempo inicial
 		std::string result = "";
+		
+		//envia os elementos a serem processados por cada processo
+		for (i = 1; i < pNumber; i++)
+		{
+			//envia os elementos a serem processados
+			//TODO
+		}
+
+		//recebe os resultados
 		for (i = 1; i < pNumber; i++)
 		{
 			char msg[100];
 			// Recebe mensagem do processo i com os valores do vetor
-			MPI_Recv(msg, , MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
+			//MPI_Recv(msg, , MPI_CHAR, i, 0, MPI_COMM_WORLD, &status);
 			//result = result + std::string(msg);
 
 			// Recebe mensagem do processo i com o valor de m
 			int aux;
-			MPI_Recv(&aux, sizeof(int), MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&aux, 1, MPI_INT, i, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			printf("%d ", i);
 			m += aux;
 
@@ -100,30 +109,9 @@ int main(int argc, char** argv) {
 			// TODO: calcular tempo e printar
 			//printf("Tempo=%.2fms\n", tempo);
 		}
-	}
-	else {
-		std::string numbers = "";
-		//cada processo lida com os indices de (pId-1)*n/pNumber até pId*n/pNumber
-		for (int i = (pId - 1) * n / pNumber; i < pId * n / pNumber && i < n; i++) {
-			if (vIn[i] != 0) {
-				//adicionar valor ao resultado
-				numbers = numbers + std::to_string(vIn[i]) + " ";
-				printf("a");
-				//incrementar contador
-				m++;
-			}
-			//envia mensagem para o processo 0 contendo os elementos a serem escritos na saida
-			MPI_Send(numbers.c_str(), strlen(numbers.c_str()) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-			//envia mensagem contendo o valor de m a ser somado
-			MPI_Send(&m, 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
-		}
-	}
 
-	// -------------------------------------------------------------------------
-	// Finalização
-
-
-	if (pId == 0) {
+		// -------------------------------------------------------------------------
+		// Escrita no arquivo de saida
 		// Cria arquivo de saída
 		arqOut = fopen(argv[2], "wt");
 
@@ -133,17 +121,54 @@ int main(int argc, char** argv) {
 		// Escreve vetor do arquivo de saída
 		for (i = 0; i < m; i++)
 		{
-			fprintf(arqOut, "%d ", vOut[i]);
+			fprintf(arqOut, result.c_str());
 		}
 		fprintf(arqOut, "\n");
 
 		// Fecha arquivo de saída
 		fclose(arqOut);
-		// Libera vetores de entrada e saída
-		free(vIn);
+
 		printf("%d\n", n);
 	}
+	else {
+		//allocate memory for arrays
+		vIn = (int*)malloc((1+(n/pNumber))* sizeof(int));
+		if (vIn == NULL)
+		{
+			printf("\nErro na alocação de memoria no processo %d\n", pId);
+			MPI_Finalize();
+			exit(1);
+		}
+		//set all values to 0
+		for (int i = 0; i < 1 + (n / pNumber); i++) {
+			vIn[i]=0;
+		}
 
+		//recebe os valores do processo 0
+		// TODO
+
+		//processa a entrada
+		std::string numbers = "";
+		for (int i = 0; i < 1 + (n / pNumber); i++) {
+			if (vIn[i] =! 0) {
+				//adicionar valor ao resultado
+				numbers = numbers + std::to_string(vIn[i]) + " ";
+				//incrementar contador
+				m++;
+			}
+		}
+		//envia mensagem para o processo 0 contendo os elementos a serem escritos na saida
+		//MPI_Send(numbers.c_str(), strlen(numbers.c_str()) + 1, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
+		//envia mensagem contendo o valor de m a ser somado
+		MPI_Send(&m, 1, MPI_INT, 0, 1, MPI_COMM_WORLD);
+
+	}
+
+	// -------------------------------------------------------------------------
+	// Finalização
+
+	// Libera vetores de entrada
+	free(vIn);
 	// Finalize the MPI environment.
 	MPI_Finalize();
 }
